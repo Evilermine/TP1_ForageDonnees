dce938f66b04b8cf809d3a4b44524c2d0a3d8255
Michael Lizotte-Gagnon <michael.lizotte-gagnon@cgi.com>
2019-02-07 19:52:47 -0500

    Optimization
    +dataset drops one time instead of 2500 times

--- _data.py
+++ _data.py
@@ -3,6 +3,8 @@
 import pandas as pd #used to read the dataset
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
 
 #Contains the data
 dataset = pd.read_csv("Datasets/NMC_5_0.csv", na_values="-9")
@@ -31,12 +33,14 @@ print("Number of countries:",dataset.nunique()['ccode'])
 # Finding null values and counting
 nullValues = dataset.isnull().any(axis=1)
 nullValueCount = 0;
-print("-----------------------------")
-print("Dropping null values' row ...")
-print("-----------------------------")
+nullValuesIndexes = []
+print("-------------------------")
+print("Dropping null-values' row ")
+print("-------------------------")
 for i in range(len(nullValues)):
     if nullValues[i]:
         nullValueCount += 1
-        dataset = dataset.drop(i)
+        nullValuesIndexes.append(i)
+dataset = dataset.drop(nullValuesIndexes)
 print("Null values:",nullValueCount)
 print("New dataset length:",dataset.size)
